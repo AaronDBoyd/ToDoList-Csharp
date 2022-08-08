c514650c865f5cc6426a1439974cd1b49f34c998
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-08 16:50:10 -0700

    add static variable and static method to Item class

--- ToDoList.Solution/ToDoList/Models/Item.cs
+++ ToDoList.Solution/ToDoList/Models/Item.cs
@@ -3,10 +3,18 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {};
+      // this static variable holds all instances of the Item class
 
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this); // add all created Item objects to _instances List 
     }   
+
+    public static List<Item> GetAll() // static method to call on Item class directly to return the static variable.
+    {
+      return _instances; 
+    }
   }
 }
