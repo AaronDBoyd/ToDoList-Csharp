0f54676e8ca7bda39df0eb0b484fa521aeebcf63
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-08 17:03:22 -0700

    create static method to clear all instances of Item class. Add teardown method to tests.

--- ToDoList.Solution/ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Solution/ToDoList.Tests/ModelTests/ItemTests.cs
@@ -6,8 +6,13 @@ using ToDoList.Models;
 namespace ToDoList.Tests 
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
