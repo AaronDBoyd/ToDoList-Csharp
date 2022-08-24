ee3a81d00c6fd4e50c31c1c2026efaf4fc214ae7
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-24 13:09:55 -0700

    add view and controller to edit items

--- ToDoList.Solution/ToDoList/Controllers/ItemsController.cs
+++ ToDoList.Solution/ToDoList/Controllers/ItemsController.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic; // allows use of Lists
-
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
@@ -38,6 +38,20 @@ namespace ToDoList.Controllers
     {
         Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
         return View(thisItem);
+    }
+
+    public ActionResult Edit(int id)
+    {
+        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+        return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+        _db.Entry(item).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
     }
   }
 }
