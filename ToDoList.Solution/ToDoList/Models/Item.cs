a536a98f42d62866fbd31bd46aa5d4eb718010b0
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-30 17:22:31 -0700

    create many-to-many relationship through CategoryItem join table/model

--- ToDoList.Solution/ToDoList/Models/Item.cs
+++ ToDoList.Solution/ToDoList/Models/Item.cs
@@ -3,10 +3,15 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    public int ItemId { get; set; }
-    public string Description { get; set; }
-    public int CategoryId { get; set; }
-    public virtual Category Category { get; set; }
+    public Item()
+        {
+            this.JoinEntities = new HashSet<CategoryItem>();
+        }
+
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<CategoryItem> JoinEntities { get;}
   }
 }
 
